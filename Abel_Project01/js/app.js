150a494b232673938f71528a532cc549b20366a3
kimo5532 <shotbyabel@gmail.com>
2015-07-07 20:53:23 -0700

    end of day one project

--- Abel_Project01/js/app.js
+++ Abel_Project01/js/app.js
@@ -1,8 +1,10 @@
 $(".start-trigger").click(function(){
   $("#start").hide();
   $("#photo1").show();
+  $("#photo-timer").text(photoStartTime);
   //this method runs my function at 1 second intervals
   setInterval(decreaseTimeBy1Second, 1000);
+  setInterval(decreasePhotoTimer, 1000);
 });
 
 // Storing the starting time in seconds into startTime variable.
@@ -19,7 +21,21 @@ function decreaseTimeBy1Second(){
   }
 }
 
+var photoStartTime = 4;
+//Jquery is accessing the document's ID and with the 'text' tag it
+// inserts the variable we created.
 
+//i want the PhotoStartTime to "start" as soon as Photo 01
+//shows on the sreen and when it reaches 0 for photo 01 to hide and results 1 to show
+function decreasePhotoTimer(){
+  if (photoStartTime === 0) {
+    $("#photo1").hide();
+    $("#results1").show();
+  } else {
+    photoStartTime = photoStartTime -1;
+    $("#photo-timer").text(photoStartTime);
+  }
+}
 
   //if start time reaches 0 seconds. stop timer and announce "game over"
 
